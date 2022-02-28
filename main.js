ec0cfe32e2d3bf29202f3347c74d480ecb5b4148
KaniSuhan <96287148+KaniSuhan@users.noreply.github.com>
2022-02-27 20:44:45 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img){block_image_object.scale towidht(700);	
+
+
+function playSound(){
+	
+}
